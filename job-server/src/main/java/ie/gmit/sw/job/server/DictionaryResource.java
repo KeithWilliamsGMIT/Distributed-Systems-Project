31335588da10052a5683c9b5d59121aa24bc9195
Keith Williams <G00324844@gmit.ie>
2017-11-22 22:15:52 +0000

    Allocate number to each request and return JSON.

--- job-server/src/main/java/ie/gmit/sw/job/server/DictionaryResource.java
+++ job-server/src/main/java/ie/gmit/sw/job/server/DictionaryResource.java
@@ -1,5 +1,9 @@
 package ie.gmit.sw.job.server;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Singleton;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,20 +14,33 @@ import javax.ws.rs.core.MediaType;
  * Handle HTTP requests sent to the /dictionary endpoint.
  */
 @Path("/dictionary")
+@Singleton
 public class DictionaryResource {
+	/*
+	 * Keep a counter of the client requests.
+	 * This will be used to allocate job numbers.
+	 */
+	private int counter = 0;
+	
 	/**
 	 * This method handles POST requests. Creates a new request and queues it for processing.
 	 * @param query, representing a word or phrase, to search for in the dictionary.
-	 * @return a message to acknowledge that the request was created and is being processed.
+	 * @return JSON containing a message which can be displayed to the user and the job number for the request.
 	 */
 	@POST
 	@Path("/{query}")
-	@Produces(MediaType.TEXT_PLAIN)
-	public String postRequest(@PathParam("query") String query) {
-		// Assign job number
+	@Produces(MediaType.APPLICATION_JSON)
+	public Map<String, Object> postRequest(@PathParam("query") String query) {
+		// Add request to inQueue.
 		
-		// Add to inQueue
+		// Create the response object.
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("message", String.format("Searching for '%s'...", query));
+		response.put("number", counter);
 		
-		return String.format("Searching for '%s'...", query);
+		// Increment the counter for next request.
+		counter++;
+		
+		return response;
 	}
 }
