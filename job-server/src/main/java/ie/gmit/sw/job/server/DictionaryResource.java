fcfbf1974159165fde4987d6160c30a54048d7e1
Keith Williams <G00324844@gmit.ie>
2017-11-22 13:24:34 +0000

    Created new Maven project with a placeholder JSP page and dictionary resource.

---
+++ job-server/src/main/java/ie/gmit/sw/job/server/DictionaryResource.java
@@ -0,0 +1,11 @@
+package ie.gmit.sw.job.server;
+
+import javax.ws.rs.Path;
+
+/**
+ * Handle HTTP requests sent to the /dictionary endpoint.
+ */
+@Path("dictionary")
+public class DictionaryResource {
+	
+}
