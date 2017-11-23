0a087217a8535d95a8d0c3ba1adc2776a807fe49
Keith Williams <G00324844@gmit.ie>
2017-11-23 10:18:42 +0000

    Implemented DictionaryService interface with placeholder functionality.

---
+++ dictionary-service/src/main/java/ie/gmit/sw/DictionaryServiceImpl.java
@@ -0,0 +1,29 @@
+package ie.gmit.sw;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * DictionaryServiceImpl is a Remote Object.
+ * Extending UnicastRemoteObject allows this remote object to communicate with the server-side skeleton.
+ * The server-side skeleton is the responsible for unmarshalling client requests on the server and marshalling responses.
+ * The remote object is used for a remote method invocation.
+ */
+public class DictionaryServiceImpl extends UnicastRemoteObject implements DictionaryService {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Default constructor for the DictionaryServiceImpl class.
+	 * @throws RemoteException
+	 */
+	private DictionaryServiceImpl() throws RemoteException { }
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String lookup(String s) throws RemoteException {
+		String definition = "String not found";
+		
+		return definition;
+	}
+}
